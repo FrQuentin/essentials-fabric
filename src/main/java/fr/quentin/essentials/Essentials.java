d4e763a99cff3e8c2236188badb091e979c5d9df
Quentin Fouilleul <fouilleul.quentin@outlook.fr>
2025-02-26 22:08:51 +0100

    Initial commit

---
+++ src/main/java/fr/quentin/essentials/Essentials.java
@@ -0,0 +1,16 @@
+package fr.quentin.essentials;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Essentials implements ModInitializer {
+	public static final String MOD_ID = "essentials";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Essentials");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/fr/quentin/essentials/EssentialsClient.java
@@ -0,0 +1,10 @@
+package fr.quentin.essentials;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EssentialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
