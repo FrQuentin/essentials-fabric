76df5e803b601352420f1c91ebae378bb24232be
Quentin Fouilleul <fouilleul.quentin@outlook.fr>
2025-03-04 20:33:52 +0100

    0.01.8

--- src/main/java/fr/quentin/essentials/mixin/MouseMixin.java
+++ src/main/java/fr/quentin/essentials/mixin/MouseMixin.java
@@ -1,8 +1,11 @@
 package fr.quentin.essentials.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
 import fr.quentin.essentials.option.ModKeyBinding;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,9 +16,21 @@ import fr.quentin.essentials.utils.ZoomManager;
 
 @Mixin(Mouse.class)
 public class MouseMixin {
-    @Inject(at = @At("RETURN"), method = "onMouseScroll(JDD)V")
+    @Inject(
+            method = "onMouseScroll(JDD)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/network/ClientPlayerEntity;isSpectator()Z"
+            ),
+            cancellable = true
+    )
     private void onMouseScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
-        ZoomManager.getInstance().onMouseScroll(vertical);
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        if (player != null && player.isSpectator() &&
+                ModKeyBinding.zoomKey.isPressed()) {
+            ci.cancel();
+            ZoomManager.getInstance().onMouseScroll(vertical);
+        }
     }
 
     @WrapWithCondition(
