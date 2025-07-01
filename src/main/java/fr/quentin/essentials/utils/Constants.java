fd87413cb9b576fcb253627a964ec94a95fe075a
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-07-01 12:06:10 +0200

    0.05.2+mc1.21.7

--- src/main/java/fr/quentin/essentials/utils/Constants.java
+++ src/main/java/fr/quentin/essentials/utils/Constants.java
@@ -12,9 +12,6 @@ public final class Constants {
     public static final double GAMMA_MIN = -10000.0;
     public static final double GAMMA_MAX = 10000.0;
 
-    // Origin Realms Address
-    public static final String ORIGIN_REALMS_ADDRESS = "play.originrealms.com";
-
     // Translation Keys
     public static final String ACCESSIBILITY_TRANSLATION_KEY = "narrator.button.accessibility";
     public static final String CREDITS_TRANSLATION_KEY = "options.credits_and_attribution";
